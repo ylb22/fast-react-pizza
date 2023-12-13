b13bf54bb212a2bdece984493b8b3cc5d2393d15
Yogesh bhoye <bhoyeyogesh0202@gmail.com>
2023-12-13 11:46:22 +0530

    fix header.jsx

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 function Username() {
   const username = useSelector((state) => state.user.username);
-  if (!username) return null;
+  if (!username) return <div></div>;
   return (
     <div className="hidden text-sm font-semibold  md:block">{username}</div>
   );
