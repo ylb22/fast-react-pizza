97d2754a2b9610537fd40ad5f6f96170566000d6
Yogesh bhoye <bhoyeyogesh0202@gmail.com>
2023-12-13 11:33:22 +0530

    first commit

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,16 @@
+import Button from "../../ui/Button";
+import { deleteItem } from "./cartSlice";
+import { useDispatch } from "react-redux";
+
+function DeleteItem({ pizzaId }) {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>
+        Delete
+      </Button>
+    </div>
+  );
+}
+
+export default DeleteItem;
